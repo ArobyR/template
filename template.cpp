6f1c07f9943a64fd7ff817c76857306ed18dd842
ArobyR <ar.rosario001@gmail.com>
2020-08-13 13:05:01 -0400

    First commit

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define forn(i, n) for (int i = 0; i < (int)(n); i++)
+#define forl(i, n) for (int i = 0; i <= (int)(n); i++) 
+#define fore(i, l, r) for (int i = (int)(l); i <= (int)(r); i++)
+#define ford(i, n) for (int i = (int) n -1; i >= 0; i--) 
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).end()
+
+using namespace std;
+
+typedef long long i64;
+typedef vector<int> vi;
+typedef vector<i64> vi64;
+typedef vector<vi> vvi;
+typedef vector<vi64> vvi64;
+typedef double ld;
+
+int main () {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.precision(10);
+	cout << fixed;
+	
+	return 0;
+}
